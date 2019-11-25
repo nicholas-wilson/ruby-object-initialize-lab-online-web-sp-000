34ca9c88a4d0e12d4507efd7ac6e35dbbeaabbc4
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-25 19:14:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,10 @@
 class Dog
 
-  attr_accessor :name
+  attr_accessor :name, :breed
 
-  def initialize(name = "Nic")
+  def initialize(name = "Nic", breed = "dog")
     @name = name
+    @breed = breed
   end
 
 end
